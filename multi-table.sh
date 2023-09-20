98adfbb470848baab6d66cebbd029c6e9e6f6331
yymin1022 <yymin1022@gmail.com>
2023-09-20 07:49:37 +0000

    Added Exception Conditions

--- multi-table.sh
+++ multi-table.sh
@@ -1,1 +1,17 @@
 #!/bin/sh
+NUM1=$1
+NUM2=$2
+
+if [ $# -ne 2 ]
+then
+	echo "Invalid Input"
+	exit 0
+fi
+
+if [ $NUM1 -lt 1 -o $NUM2 -lt 1 ]
+then
+	echo "Input must be greater than 0"
+	exit 0
+fi
+
+
